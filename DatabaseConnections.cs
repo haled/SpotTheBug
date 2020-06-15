da17e28c905a0720dcfcdb55f4b3748a3ce35122
Darren Hale <darren@Darrens-MacBook-Air.local>
2020-06-15 09:46:56 -0500

    Added a few more ideas and started creating code samples.

---
+++ DatabaseConnections.cs
@@ -0,0 +1,30 @@
+public DbConnection _connection;
+
+public bool OpenDatabase(string connString)
+{
+    bool result = false;
+
+    if(connString != null && !string.IsNullOrEmpty(connString))
+    {
+	_connection = new SqlConnection(connString);
+	result = true;
+    }
+
+    return result;
+}
+
+public bool CloseDatabase()
+{
+    _connection.Close();
+    return true;
+}
+
+// Buggy Code
+public void WorkWithData()
+{
+    var isDatabaseOpen = OpenDatabase(_connString);
+
+    // Do something with the database
+
+    var isDatabaseClosed = CloseDatabase();
+}
